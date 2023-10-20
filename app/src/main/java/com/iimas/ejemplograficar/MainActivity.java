4a72d490ce06cbb8ec76d41f563ef420df8beb98
MathICIMAF <ahmedlp9@gmail.com>
2023-10-20 09:46:56 -0600

    Initial commit

---
+++ app/src/main/java/com/iimas/ejemplograficar/MainActivity.java
@@ -0,0 +1,107 @@
+package com.iimas.ejemplograficar;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.EditText;
+
+import com.github.mikephil.charting.charts.LineChart;
+import com.github.mikephil.charting.components.YAxis;
+import com.github.mikephil.charting.data.Entry;
+import com.github.mikephil.charting.data.LineData;
+import com.github.mikephil.charting.data.LineDataSet;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    LineChart lineChart;
+    int N = 100;
+    int M = 256;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        lineChart = findViewById(R.id.lineChart);
+
+        EditText nEdit = findViewById(R.id.nEdit);
+        EditText mEdit = findViewById(R.id.mEdit);
+
+        nEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try {
+                    N = Integer.parseInt(charSequence.toString());
+                    inicializarGrafica();
+                    lineChart.invalidate();
+                }
+                catch (Exception e){}
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        mEdit.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                try {
+                    M = Integer.parseInt(charSequence.toString());
+                    inicializarGrafica();
+                    lineChart.invalidate();
+                }
+                catch (Exception e){}
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        inicializarGrafica();
+    }
+
+    private void inicializarGrafica() {
+
+        lineChart.setBackgroundColor(Color.BLACK);
+
+        YAxis yAxis = lineChart.getAxisRight();
+        yAxis.setTextColor(Color.WHITE);
+
+        List<Entry> entries = new ArrayList<>();
+
+        for (int i = 0; i < N; i++){
+            float y = (float) Math.sin(2*Math.PI*i*N/M);
+            Entry entry = new Entry(i,y);
+            entries.add(entry);
+        }
+
+        LineDataSet lineDataSet = new LineDataSet(entries,"Frecuencia Media");
+        lineDataSet.setColor(Color.WHITE);
+        lineDataSet.setLineWidth(2);
+        lineDataSet.setDrawCircles(false);
+
+        LineData lineData = new LineData(lineDataSet);
+        lineChart.setData(lineData);
+
+    }
+}
